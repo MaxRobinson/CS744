e1ecafd669ddbe3c1ff92cb381d96377b2b7cc1e
MaxRobinson <maxrgit@gmail.com>
2018-09-18 00:56:13 -0400

    Module3 short problem set & module 2 program

--- Module2/module2/src/main/java/edu/jhu/mrobi100/Ingest.java
+++ Module2/module2/src/main/java/edu/jhu/mrobi100/Ingest.java
@@ -1,10 +1,9 @@
 package edu.jhu.mrobi100;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.logging.Logger;
-import java.util.regex.Matcher;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class Ingest {
@@ -12,35 +11,73 @@ public class Ingest {
   private final String startParagraph = "<P ID=";
   private final Pattern start = Pattern.compile(startParagraph);
   private final Pattern stop = Pattern.compile("</P>");
+  private final List<String> punct =
+      Arrays.asList(".", ",", "'", "?", "!", ":", ";", "(", ")", "[", "]", "{", "}", "&", "|", "-");
+
+  private final Postings postings;
+  private final Lexicon lexicon;
+
+  private int numberOfDocuments = 0;
+  private int numberOfTerms = 0;
 
   public Ingest(String fileName) throws Exception {
     this.fileName = fileName;
+    this.postings = new Postings();
+    this.lexicon = new Lexicon();
   }
 
-  public void read() {
+  public Lexicon read() throws IOException {
     try (BufferedReader bf = new BufferedReader(new FileReader(fileName))) {
       String line;
+      StringBuilder document = new StringBuilder();
       int id = -1;
       while ((line = bf.readLine()) != null) {
 
         line = line.trim();
 
         if (start.matcher(line).find()) {
-          parseStart(line);
+          id = parseStart(line);
         } else if (stop.matcher(line).find()) {
-          parseStop(line, id);
+          updateTerms(document.toString(), id);
+          document.delete(0, document.length());
+          id = -1;
+          numberOfDocuments+=1;
         } else {
-
+          document.append(line);
         }
       }
 
     } catch (Exception ex) {
-
+      throw ex;
     }
+
+    return lexicon;
   }
 
-  private String parseStop(String line, int docId) {
-    return null;
+  private void updateTerms(String line, int id) {
+    HashMap<String, Integer> termFreq = new HashMap<>();
+
+    String[] tokens = line.split(" ");
+    for (String token : tokens) {
+      token = token.toLowerCase();
+      if (punct.contains(token)) {
+        continue;
+      }
+
+      numberOfTerms+=1;
+      Integer df = termFreq.get(token);
+      if(df == null){
+        termFreq.put(token, 1);
+      } else {
+        termFreq.put(token, df + 1);
+      }
+    }
+
+    for(Map.Entry<String, Integer> entry: termFreq.entrySet()){
+      String key = entry.getKey();
+      Integer df = entry.getValue();
+      lexicon.addTerm(key, id, df);
+    }
   }
 
   private int parseStart(String line) {
@@ -53,4 +90,16 @@ public class Ingest {
     }
     return id;
   }
+
+  // <editor-fold desc="Accessors">
+
+  public int getNumberOfDocuments() {
+    return numberOfDocuments;
+  }
+
+  public int getNumberOfTerms() {
+    return numberOfTerms;
+  }
+
+  // </editor-fold>
 }
--- Module2/module2/src/main/java/edu/jhu/mrobi100/Module2.java
+++ Module2/module2/src/main/java/edu/jhu/mrobi100/Module2.java
@@ -2,7 +2,22 @@ package edu.jhu.mrobi100;
 
 
 public class Module2 {
-  public static void main(String[] args) {
+  private static String invertedFileName = "/home/max/Documents/JHU-Masters/InformationRetrieval" +
+          "/Module2/module2/src/main/java/edu/jhu/mrobi100/invertedFile.txt";
+  private static String lexiconFileName = "/home/max/Documents/JHU-Masters/InformationRetrieval" +
+          "/Module2/module2/src/main/java/edu/jhu/mrobi100/lexicon.txt";
 
+
+  public static void main(String[] args) throws Exception {
+    // Create Lexicon and Inverted File
+    Ingest ingest = new Ingest(Module2.class.getResource("headlines.txt").toURI().toString().substring(5));
+    Lexicon lexicon = ingest.read();
+
+    lexicon.writeInvertedFile(invertedFileName);
+    lexicon.writeLexicon(lexiconFileName);
+
+    System.out.println("Number of Documents: " + ingest.getNumberOfDocuments());
+    System.out.println("Number of unique terms: " + lexicon.getDict().keySet().size());
+    System.out.println("Number of terms observed: " + ingest.getNumberOfTerms());
   }
 }
